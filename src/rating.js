3516a010447c5421adf1c9c1aad8d8ecae4769f9
beyond-th0ught <yadavkhushboo7653@gmail.com>
2022-06-30 01:29:47 +0530

    Same Commit

--- src/rating.js
+++ src/rating.js
@@ -1,29 +1,12 @@
 import React, { useState } from 'react'
-import {Rating} from 'react-simple-star-rating'
-
 export default function App() {
   const [rating, setRating] = useState(0) // initial rating value
 
-  // Catch Rating value
-  const handleRating = (rate) => {
-    setRating(rate)
-    // Some logic
-  }
+  // Catch Rating valu
 
   return (
     <div className='App'>
-      <Rating
-        className='flex'
-        onClick={handleRating}
-        ratingValue={rating}
-        size={20}
-        label
-        transition
-        fillColor='orange'
-        emptyColor='gray'
-     // Will remove the inline style if applied
-      />
-      <div >{rating}</div>
+    
     </div>
   )
 }
