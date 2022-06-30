0196904985b4094dd451656231465f6e58190e3f
beyond-th0ught <yadavkhushboo7653@gmail.com>
2022-06-30 15:56:22 +0530

    fhv

--- src/subscribe.js
+++ src/subscribe.js
@@ -2,17 +2,24 @@ import React from 'react'
 
 export default function subscribe() {
   return (
-    <div>
-        <div>
-            Join our newsletter and get
-            $20 discount for your first order
-        </div>
-        <div>
-            <input alt="Enter your name" class="bg-slate-500">
+    <div class=" flex flex-col justify-center items-center">
+    <div class="text-center text-3xl font-bold text-blue-900 mt-16">
+            <div>
+              Join our newsletter and get
+            </div>
+            <div>
+              $20 discount for your first order
+            </div>
+    </div>
+        
+        <div class="text-center mt-7 w-full">
+            <input placeholder='Enter you email addesee'  alt="Enter your name" class=" text-xs text-blue-900 w-[37%] p-3 rounded-full bg-slate-200">
 
             </input>
         </div>
-        <div></div>
+        <div  class=" mt-5 bg-[#ffa600] w-[12%] py-2 rounded-full text-white text-center font-bold cursor-pointer text-sm">
+           Subscribe
+        </div>
     </div>
   )
 }
